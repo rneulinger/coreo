b728358dfcad0fab9bc85aa115caa8a97fe0a1de
rneulinger <rneulinger@ext.blackned.de>
2026-02-08 07:47:45 +0100

    utilities

--- src/main/scala/coreo/PwApp.scala
+++ src/main/scala/coreo/PwApp.scala
@@ -102,14 +102,14 @@ class PwApp(val baseUrl: String) extends AnyApp {
         ""
       }
     }
-    val ridux =
+    val TsApp =
       s"""namespace Generic{
          |
          |//using Core;
          |
          |// ReSharper disable InconsistentNaming
          |
-         |export class Ridux extends Core.ROOT
+         |export class TsApp extends Core.ROOT
          |{
        ${decl.toList.filter(_.nonEmpty).sorted.distinct.mkString("\n")}
          |constructor( baseUrl:String ){
@@ -118,11 +118,11 @@ class PwApp(val baseUrl: String) extends AnyApp {
          |}
          |}
          |}""".stripMargin
-    writeTextToFile(ridux, "build/Ridux.ts")
+    writeTextToFile(TsApp, "build/TsApp.ts")
 
   }
 
-  def genAllCs(): Unit = {
+  def genAllCs(appName:String): Unit = {
     val decl = for (f <- frms) yield {
       val fn = f._2.myType + "_.cs"
       val code = Formatter(f._2).mkCs
@@ -145,14 +145,14 @@ class PwApp(val baseUrl: String) extends AnyApp {
         ""
       }
     }
-    val ridux =
+    val csApp =
       s"""namespace Generic;
          |
          |using Core;
          |
          |// ReSharper disable InconsistentNaming
          |
-         |export class Ridux : ROOT
+         |export class ${appName} : ROOT
          |{
        ${decl.toList.filter(_.nonEmpty).sorted.distinct.mkString("\n")}
          |constructor( string baseUrl ){
@@ -160,7 +160,7 @@ class PwApp(val baseUrl: String) extends AnyApp {
          |}
          |}
          |""".stripMargin
-    writeTextToFile(ridux, "build/Ridux.cs")
+    writeTextToFile(csApp, s"build/$appName.cs")
   }
 
   lazy val gotos = short
@@ -189,16 +189,16 @@ class PwApp(val baseUrl: String) extends AnyApp {
    *
    * @return
    */
-  def genCsToBuild() = {
+  def genCsToBuild(appName:String) = {
     val dest = "build/cs/"
 
-    def dropRidux(s: String) =
-      if s.startsWith("ridux.") then s.substring(6) else s
+    def dropAppPrefix(s: String) =
+      if s.startsWith(s"$appName.") then s.substring(appName.length + 1) else s
 
     def allPacks: List[String] = {
       val res = for (frm <- short) yield frm._2.getClass.getPackage.getName
 
-      res.toList.distinct.sorted.filterNot(_ == "coreo").map(dropRidux(_))
+      res.toList.distinct.sorted.filterNot(_ == "coreo").map(dropAppPrefix(_))
     }
 
     def mkDirectory(any: Any): String = {
@@ -210,7 +210,7 @@ class PwApp(val baseUrl: String) extends AnyApp {
     for (frm <- short) {
       java.io.File(dest).mkdirs()
       val code =
-        s"""namespace ${"Ridux." + dropRidux(frm._2.getClass.getPackage.getName)};
+        s"""namespace ${s"$appName." + dropAppPrefix(frm._2.getClass.getPackage.getName)};
            |using Coreo;
            |
            |${Formatter(frm._2).mkCs}
@@ -246,7 +246,7 @@ class PwApp(val baseUrl: String) extends AnyApp {
 
       val app =
         s"""
-           |namespace Ridux;
+           |namespace $appName;
            |using Microsoft.Playwright;
            |using Coreo;
            |
@@ -263,11 +263,11 @@ class PwApp(val baseUrl: String) extends AnyApp {
            |
            |}
            |""".stripMargin
-      writeTextToFile(app, dest + "Ridux/App.cs")
+      writeTextToFile(app, dest + s"$appName/App.cs")
     }
   }
 
-  def mkMermaids() = {
+  def mkMermaids(appName:String) = {
     for (frm <- short) {
       val path = "build/mermaid/" + frm._2.getClass.getPackageName.replace(".", "/")
       println(path)
@@ -281,7 +281,7 @@ class PwApp(val baseUrl: String) extends AnyApp {
     val incs = for (frm <- short.values.toList.sortBy(_.getClass.getName)) yield {
       val path = "" + frm.getClass.getName.replace(".", "/")
       val cn = frm.getClass.getName
-      val name = if cn.startsWith("ridux.") then cn.drop(6) else cn
+      val name = if cn.startsWith(s"$appName.") then cn.drop(6) else cn
       s"""== $name
          |[mermaid]
          |----
@@ -299,40 +299,6 @@ class PwApp(val baseUrl: String) extends AnyApp {
          |""".stripMargin
     writeTextToFile(adoc, s"build/mermaid/Mermaids.adoc")
   }
-
-  // dir *.scala /b/s > out.txt
-  def checkDialogs(): Unit = {
-    val filePath = "src/main/scala/ridux/out.txt"
-    val source = Source.fromFile(filePath)
-    try {
-      val content = source
-        .mkString
-        .replace("\\", ".")
-        .replace("C:.Projects.playwrightriduxnodecore.src.main.scala.", "")
-        .replace(".scala", "")
-        .linesIterator
-        .filter(!_.endsWith(".tree")).toSet
-
-      val registered = short.values.map(_.Self).toSet
-      for (dlg <- content)
-        if registered.contains(dlg) then {} //println( "hit " + dlg )
-        else println("mis " + dlg)
-    } finally {
-      source.close()
-    }
-
-    //  val files = Source.
-    //    fromFile( filepath).
-    //    mkString()
-    //    lines().
-    //    filterNot(_.endsWith("tree.scala")).
-    //    toSet
-    //
-    //
-    //  println(files.toList.sorted)
-    //  println(registered.toList.sorted)
-  }
-  
 }
 
 
