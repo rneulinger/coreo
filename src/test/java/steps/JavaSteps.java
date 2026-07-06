cbc210dfb2c4f35d5a3278ce9767af1a2ce974f4
robert neulinger <robert@neulinger.info>
2026-07-06 06:42:53 +0200

    up to two params with trailing text

--- src/test/java/steps/JavaSteps.java
+++ src/test/java/steps/JavaSteps.java
@@ -6,26 +6,59 @@ import io.cucumber.java8.En;
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JavaSteps implements En {
-    void log( String... args){
-        for( String arg:args){
-            System.out.print(arg + ":");
+    void log( Object... args){
+        for( Object arg:args){
+            System.out.print(">" + arg.toString() + "< ");
         }
         System.out.println();
     }
     public JavaSteps() {
 
-        // simple; no parameter
-        And("^# ([^\"]+[^:])$", (String t1) -> {
+        // no parameter
+        And("^# ([^\"]*[^:])$", (String t1) -> {
             log(t1);
         });
+        // no parameter and table
         And("^# ([^\"]+)(?::)$", (String t1, Object o) -> {
             log(t1, o.toString());
         });
-        And("^# ([^\"]+) \"([^\"]*)\"([^\"]*)$", (String t1, String p1, String t2) -> {
+
+        // one parameter no trailing text
+        And("^# ([^\"]+) \"([^\"]*)\"$", (String t1, String p1) -> {
+            log(t1, p1);
+        });
+        // one parameter no trailing text and table
+        And("^# ([^\"]+) \"([^\"]*)\"(?::)$", (String t1, String p1, Object o) -> {
+            log(t1, p1, o);
+        });
+
+        // one parameter with trailing text
+        And("^# ([^\"]+) \"([^\"]*)\" ([^\"]*[^:])$", (String t1, String p1, String t2) -> {
             log(t1, p1, t2);
         });
-        And("^# ([^\"]+) \"([^\"]*)\"([^\"]*) \"([^\"]*)\"([^\"]*)$", (String t1, String p1, String t2, String p2, String t3) -> {
+        // one parameter with trailing text and table
+        And("^# ([^\"]+) \"([^\"]*)\" ([^\"]*)(?::)$", (String t1, String p1, String t2, Object o) -> {
+            log(t1, p1, t2, o);
+        });
+
+        // two parameters no trailing text
+        And("^# ([^\"]+) \"([^\"]*)\"([^\"]*) \"([^\"]*)\"$", (String t1, String p1, String t2, String p2 ) -> {
+            log(t1, p1, t2, p2);
+        });
+
+        // two parameters no trailing text and table
+        And("^# ([^\"]+) \"([^\"]*)\"([^\"]*) \"([^\"]*)\"(?::)$", (String t1, String p1, String t2, String p2, Object o) -> {
+            log(t1, p1, t2, p2, o);
+        });
+
+        // two parameters with trailing text
+        And("^# ([^\"]+) \"([^\"]*)\"([^\"]*) \"([^\"]*)\"([^\"]*[^:])$", (String t1, String p1, String t2, String p2, String t3) -> {
             log(t1, p1, t2, p2, t3);
+        });
+
+        // two parameters with trailing text and table
+        And("^# ([^\"]+) \"([^\"]*)\"([^\"]*) \"([^\"]*)\"([^\"]+)(?::)$", (String t1, String p1, String t2, String p2, String t3, Object o) -> {
+            log(t1, p1, t2, p2, t3, o);
         });
 
         And("goTo {string}", (String string) -> {
@@ -69,16 +102,16 @@ public class JavaSteps implements En {
             throw new io.cucumber.java8.PendingException();
         });
 
-        When("click text {string}", (String string) -> {
+        And("click text {string}", (String string) -> {
             // Write code here that turns the phrase above into concrete actions
             throw new io.cucumber.java8.PendingException();
         });
 
-        When("set {string} = {string}", (String string, String string2) -> {
+        And("set {string} = {string}", (String string, String string2) -> {
             // Write code here that turns the phrase above into concrete actions
             throw new io.cucumber.java8.PendingException();
         });
-        Given("expect:", (io.cucumber.datatable.DataTable dataTable) -> {
+        And("expect:", (io.cucumber.datatable.DataTable dataTable) -> {
             // Write code here that turns the phrase above into concrete actions
             // For automatic transformation, change DataTable to one of
             // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
@@ -88,7 +121,7 @@ public class JavaSteps implements En {
             // For other transformations you can register a DataTableType.
             throw new io.cucumber.java8.PendingException();
         });
-        Given("expect: {string}", (String string, io.cucumber.datatable.DataTable dataTable) -> {
+        And("expect: {string}", (String string, io.cucumber.datatable.DataTable dataTable) -> {
             // Write code here that turns the phrase above into concrete actions
             // For automatic transformation, change DataTable to one of
             // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
--- src/test/resources/Examples.feature
+++ src/test/resources/Examples.feature
@@ -4,20 +4,32 @@ Feature: Examples
     * # And
     When onTo "File new"
 
-     * goTo "abc"
-     * # das is
-    * # das is:
-    | key | value |
-    | Name| Doe   |
-    * # das is:
+    * goTo "abc"
+    * # no parameter
+    * # no parameter + table:
+      | key | value |
+      | Name| Doe   |
+    * # no parameter + multiline text:
       """
       das ist ein
       mehrzeiliger
       text
       """
-    * # das ist "Unsinn"
-    * # das ist "Unsinn" und
-    * # das ist "Unsinn" und "noch"
-    * # das ist "Unsinn" und "noch" mehr unsinn
+
+    * # one parameter, no trailing text "p1"
+    * # one parameter, no trailing text + object "p1":
+    |table or text|
+
+    * # one parameter "p1" with trailing text
+    * # one parameter "p1" with trailing text plus table:
+    |table or text|
+
+    * # two parameters "p1" no trailing text "p2"
+    * # two parameters "p1" no trailing text + object "p2":
+      |table or text|
+
+    * # two parameters "p1" and "p2" with trailing text
+    * # two parameters "p1" and "p2" with trailing text + object:
+      |table or text|
 
 
